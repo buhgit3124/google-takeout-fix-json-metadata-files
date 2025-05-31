948272d1c95c330de00f0fd85fb7a86695e2aba4
buhgit3124 <buhgit3124@gmail.com>
2025-05-31 11:22:11 -0700

    FSDFDS

--- google-takeout-fix-json-metadata-files.py
+++
@@ -1,8 +0,0 @@
-# google-takeout-fix-json-metadata-files
-# created by Théophane Mayaud for use when downloading large libraries
-# from google photos, through / using Google Takeout (takeout.google.com)
-
-# Use this freely, you can simply run it with the first argument being the
-# directory to scan and correct
-
-
